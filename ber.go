494c239121d24f09b5507dd18e2c4e2ddbeff63d
Nick F <nick@nickf.net>
2016-09-25 17:40:18 +0100

    update

--- ber.go
+++ ber.go
@@ -134,7 +134,7 @@ func encodeLength(out *bytes.Buffer, length int) (err error) {
 }
 
 func readObject(ber []byte, offset int) (asn1Object, int, error) {
-	//fmt.Printf("\n====> Starting readObject at offset: %d\n\n", offset)
+	fmt.Printf("\n====> Starting readObject at offset: %d\n\n", offset)
 	tagStart := offset
 	b := ber[offset]
 	offset++
