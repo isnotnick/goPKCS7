60bdf1983e432c9108bcef1315985007e0b308b3
Nick F <isnotnick@users.noreply.github.com>
2016-09-26 13:06:59 +0100

    Update ber.go

--- ber.go
+++ ber.go
@@ -182,7 +182,7 @@ func readObject(ber []byte, offset int) (asn1Object, int, error) {
 		}
 	} else if l == 0x80 {
 		// find length by searching content
-		fmt.Printf("Searching for end of indefinite object. Object starts at: %d, search begins from %d\n", offset-2, len(ber)+2)
+		//fmt.Printf("Searching for end of indefinite object. Object starts at: %d, search begins from %d\n", offset-2, len(ber)+2)
 		markerIndex := bytes.LastIndex(ber[offset:], []byte{0x0, 0x0})
 		if markerIndex == -1 {
 			return nil, 0, errors.New("ber2der: Invalid BER format")
