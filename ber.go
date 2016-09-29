cbd383de6b10134dc3f37a282cde05d9e761c4a4
Nick F <nick@nickf.net>
2016-09-29 14:17:17 +0100

    .

--- ber.go
+++ ber.go
@@ -362,7 +362,7 @@ func readObjectForIndefCount(ber []byte, offset int) (asn1Object, int, error) {
 			markerIndex = 0
 		}
 		fmt.Println("GOT ONE...at: ", markerIndex)
-		eocCount = Append(eocCount, markerIndex)
+		eocCount = append(eocCount, markerIndex)
 		length = markerIndex
 		hack = 2
 		//fmt.Printf("--> (compute length) marker found at offset: %d\n", markerIndex+offset)
