669a379874cb9694ea0e746ee244a4d9463f5083
Nick F <isnotnick@users.noreply.github.com>
2016-10-13 10:33:52 +0100

    Update pkcs7.go

--- pkcs7.go
+++ pkcs7.go
@@ -167,10 +167,10 @@ func parseSignedData(data []byte) (*PKCS7, error) {
 	}
 	// Compound octet string
 	if compound.IsCompound {
-		fmt.Println("IS COMPOUND AND SIZE: ", len(compound.Bytes))
 		if _, err = asn1.Unmarshal(compound.Bytes, &content); err != nil {
 			return nil, err
 		}
+		fmt.Println("IS COMPOUND AND SIZE: ", len(compound.Bytes))
 	} else {
 		// assuming this is tag 04
 		content = compound.Bytes
